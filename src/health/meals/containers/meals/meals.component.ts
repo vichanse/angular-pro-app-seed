4125d6aa3fcbd6940a87c61724971f66f1381d89
vicky.nsenga <Keylia2010>
2017-10-12 05:25:55 +0200

    Async Pipe "as" syntax and routing

--- src/health/meals/containers/meals/meals.component.ts
+++ src/health/meals/containers/meals/meals.component.ts
@@ -10,8 +10,32 @@ import { Store } from 'store';
   selector: 'meals',
   styleUrls: ['meals.component.scss'],
   template: `
-    <div>
-      {{ meals$ | async | json }}
+    <div class="meals">
+      <div class="meals__title">
+        <h1>
+          <img src="/img/food.svg">
+          Your meals
+        </h1>
+        <a
+          class="btn__add"
+          [routerLink]="['../meals/new']">
+          <img src="/img/add-white.svg">
+          New Meal
+        </a>
+      </div>
+      <div *ngIf="meals$ | async as meals; else loading;">
+        <div class="message" *ngIf="!meals.length">
+          <img src="/img/face.svg">
+          No meals, add a new meal to start
+        </div>
+        <!-- meals ngFor -->
+      </div>
+      <ng-template #loading>
+        <div class="message">
+          <img src="/img/loading.svg">
+          Fetching meals...
+        </div>
+      </ng-template>
     </div>
   `
 })
